b1446dc2553b67d7cfd368880ad26e31ec5f6bfa
Olga-Olga <olga_l@ukr.net>
2023-08-09 08:59:56 +0300

    commit

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,42 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import PropTypes from 'prop-types';
+i;
 
 const modalRoot = document.querySelector('#modal-root');
-export class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscape);
-  }
+export const Modal = () => {
+  // componentDidMount() {
+  //   window.addEventListener('keydown', this.handleEscape);
+  // }
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEscape);
-  }
+  // componentWillUnmount() {
+  //   window.removeEventListener('keydown', this.handleEscape);
+  // }
 
-  handleEscape = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
-    }
-  };
+  // handleEscape = e => {
+  //   if (e.code === 'Escape') {
+  //     this.props.onClose();
+  //   }
+  // };
 
   handleBackDropClick = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
+      // this.props.onClose();
     }
   };
 
-  render() {
-    return createPortal(
-      <div>
-        <div className="overlay" onClick={this.handleBackDropClick}>
-          <div className="modal">{this.props.children}</div>
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
-}
-
-Modal.propTypes = {
-  onClose: PropTypes.func.isRequired,
-  children: PropTypes.node.isRequired,
+  // render() {
+  return createPortal(
+    <div>
+      <div className="overlay" onClick={handleBackDropClick}>
+        <div className="modal">{props.children}</div>
+      </div>
+    </div>,
+    modalRoot
+  );
+  // }
 };
+
+// Modal.propTypes = {
+//   onClose: PropTypes.func.isRequired,
+//   children: PropTypes.node.isRequired,
+// };
